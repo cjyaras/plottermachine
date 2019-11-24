5bbd68f77807f45b680d13c848a89ffe6752b404
John Yaras <cy123@duke.edu>
2019-11-24 15:47:20 -0500

    added char buffer

---
+++ char_buffer.v
@@ -0,0 +1,28 @@
+module char_buffer(next_character, CHAR_READY, index, curr_character, END_OF_BUFFER);
+	input [7:0] next_character;
+	input CHAR_READY;
+	input [6:0] index;
+	output [7:0] curr_character;
+	output END_OF_BUFFER;
+	
+	reg [799:0] buffer;
+	reg END_OF_BUFFER;
+	
+	assign curr_character = buffer[8*index +: 8];
+	
+	initial begin
+		buffer <= 800'b0;
+		END_OF_BUFFER = 1'b0;
+	end
+	
+	always @(posedge CHAR_READY) begin
+		if (next_character != 8'b0) begin
+			buffer[8*index +: 8] <= next_character;
+		end
+		
+		else begin
+			END_OF_BUFFER <= 1;
+		end
+	end
+	
+endmodule
--- output_files/plottermachine.cdf
+++ output_files/plottermachine.cdf
@@ -4,7 +4,7 @@ JedecChain;
 	DefaultMfr(6E);
 
 	P ActionCode(Cfg)
-		Device PartName(EP4CE115F29) Path("C:/intelFPGA_lite/17.1/plottermachine/output_files/") File("plottermachine.sof") MfrSpec(OpMask(1));
+		Device PartName(EP4CE115F29) Path("C:/Users/cy123/Desktop/plottermachine/output_files/") File("plottermachine.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
