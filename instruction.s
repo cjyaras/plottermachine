7587fe8976bb3dfb78b23be781b1c747b5c6fb99
Januario Carreiro <jjc70@duke.edu>
2019-12-04 17:11:39 -0500

    Added parts list

---
+++ instruction.s
@@ -0,0 +1,9 @@
+.data
+
+.text
+	# set r1, r2, r3 to 0
+	add $r1, $r0 , $r0	# current angle
+	add $r2, $r0 , $r0	# current x-pos
+	add $r3, $r0 , $r0	# current y-pos
+	# make sure X less than 2250, Y less than 1550
+
